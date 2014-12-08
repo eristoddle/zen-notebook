ccdb8c8c621449e94efc88ebc988bab8992dd07c
eristoddle <stephanmil@gmail.com>
2014-12-08 13:20:37 -0600

    Release v0.0.12

--- scripts/releaseBuilds.sh
+++ scripts/releaseBuilds.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+echo "Zipping and moving executables"
 cd ..
 cd "build/Zen Notebook"
+ls
 zip linux-x32.zip linux32
 zip linux-x64.zip linux64
 zip mac.zip osx
 zip windows.zip win
-mv linux-x32.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/linux-x32.zip
-mv linux-x64.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/linux-x64.zip
-mv mac.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/mac.zip
-mv windows.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/windows.zip
+#mv linux-x32.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/linux-x32.zip
+#mv linux-x64.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/linux-x64.zip
+#mv mac.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/mac.zip
+#mv windows.zip ../../../../xampp/htdocs/debian/af23/sites/default/files/software/windows.zip
