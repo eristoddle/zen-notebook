fa680d837ea513f9aaaf960b73aef077dcec7ab5
Stephan Miller <stephanmil@gmail.com>
2016-09-10 00:33:22 -0500

    Commit before major update

--- server/vendor/bin/classpreloader.php
+++ server/vendor/bin/classpreloader.php
@@ -1,1 +1,11 @@
-../classpreloader/classpreloader/classpreloader.php
+#! /usr/bin/env php
+<?php
+
+if (file_exists($autoloadPath = __DIR__ . '/../../autoload.php')) {
+    require_once $autoloadPath;
+} else {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+$application = new ClassPreloader\Application();
+$application->run();
