33b16ebd5b5574aa4e488124a94739bc9091812e
Stephan Miller <stephanmil@gmail.com>
2016-09-15 23:18:26 -0500

    npm update

--- app/pages/notebook/notebook.ts
+++ app/pages/notebook/notebook.ts
@@ -1,11 +1,67 @@
 import {Component} from '@angular/core';
+import {ModalController, Platform, NavParams, ViewController} from 'ionic-angular';
 
 
 @Component({
-  templateUrl: 'build/pages/notebook/notebook.html'
+    templateUrl: 'build/pages/notebook/notebook.html'
 })
 export class Notebook {
-  constructor() {
+    constructor(public modalCtrl: ModalController) { }
 
-  }
+    openModal(characterNum) {
+
+        let modal = this.modalCtrl.create(ModalsContentPage, characterNum);
+        modal.present();
+    }
+}
+
+@Component({
+    templateUrl: './build/pages/notebook/modals/modal-content.html'
+})
+class ModalsContentPage {
+    character;
+
+    constructor(
+        public platform: Platform,
+        public params: NavParams,
+        public viewCtrl: ViewController
+    ) {
+        var characters = [
+            {
+                name: 'Gollum',
+                quote: 'Sneaky little hobbitses!',
+                image: 'img/avatar-gollum.jpg',
+                items: [
+                    { title: 'Race', note: 'Hobbit' },
+                    { title: 'Culture', note: 'River Folk' },
+                    { title: 'Alter Ego', note: 'Smeagol' }
+                ]
+            },
+            {
+                name: 'Frodo',
+                quote: 'Go back, Sam! I\'m going to Mordor alone!',
+                image: 'img/avatar-frodo.jpg',
+                items: [
+                    { title: 'Race', note: 'Hobbit' },
+                    { title: 'Culture', note: 'Shire Folk' },
+                    { title: 'Weapon', note: 'Sting' }
+                ]
+            },
+            {
+                name: 'Samwise Gamgee',
+                quote: 'What we need is a few good taters.',
+                image: 'img/avatar-samwise.jpg',
+                items: [
+                    { title: 'Race', note: 'Hobbit' },
+                    { title: 'Culture', note: 'Shire Folk' },
+                    { title: 'Nickname', note: 'Sam' }
+                ]
+            }
+        ];
+        this.character = characters[this.params.get('charNum')];
+    }
+
+    dismiss() {
+        this.viewCtrl.dismiss();
+    }
 }
