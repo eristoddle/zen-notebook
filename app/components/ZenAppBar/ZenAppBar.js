4c3ccc82550076474e31d08a94aa62aa2c84308f
Stephan MIller <stephanmil@gmail.com>
2017-01-02 21:20:02 -0600

    more material ui

--- app/components/ZenAppBar/ZenAppBar.js
+++ app/components/ZenAppBar/ZenAppBar.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
-/**
- * A simple example of `AppBar` with an icon on the right.
- * By default, the left icon is a navigation-menu.
- */
-const ZenAppBar = () => (
-  <AppBar
-    title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-);
+export default class ZenAppBar extends React.Component {
 
-export default ZenAppBar;
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+    return (
+        <div>
+            <AppBar
+                title="Zen Notebook"
+                onLeftIconButtonTouchTap={this.handleToggle}
+                />
+            <Drawer open={this.state.open}>
+                <MenuItem>
+                    Menu Item
+                </MenuItem>
+                <MenuItem>
+                    Menu Item 2
+                </MenuItem>
+            </Drawer>
+        </div>
+    );
+  }
+}
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,7 +1,10 @@
 // @flow
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ZenAppBar from '../components/ZenAppBar/ZenAppBar.js';
+
+injectTapEventPlugin();
 
 export default class App extends Component {
   props: {
