e987f2e906dfccd98f1c9033e31eb3ebe0748d23
Stephan MIller <stephanmil@gmail.com>
2017-01-28 15:19:13 -0600

    working so far with redux

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import counter from './counter';
+import notebook from './notebook'
 
 const rootReducer = combineReducers({
   counter,
-  routing
+  routing,
+  notebook
 });
 
 export default rootReducer;
