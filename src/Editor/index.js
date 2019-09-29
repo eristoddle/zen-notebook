239406d8f2f3530ed96df22b347d54a0fac7c144
Stephan MIller <stephanmil@gmail.com>
2019-09-28 22:09:44 -0500

    eject react-scripts

---
+++ src/Editor/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ContentEditable from 'react-contenteditable'
+
+export default function Editor() {
+      const [html, setHtml] = React.useState("");
+      const refContentEditable = React.useRef(null);
+      console.log('html', html);
+    return (
+        <ContentEditable
+              innerRef={refContentEditable}
+              html={html}
+              disabled={false}
+              onChange={(e) => setHtml(e.target.value)}
+              tagName='article'
+            />
+    );
+}
