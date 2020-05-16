f412d21dca90521d5f215eb27fe6f194c1d97f41
Stephan Miller <stmiller@shamrocktradingcorp.com>
2020-05-15 22:54:14 -0500

    fix react navigation

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
