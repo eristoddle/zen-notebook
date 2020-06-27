e523b529004ad65142a77c5eb0c133c582a5c0f4
Stephan Miller <stmiller@shamrocktradingcorp.com>
2020-06-27 00:39:04 -0500

    Run Expo Update and Web and Mobile are Good

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -4,6 +4,6 @@ const merge = require('webpack-merge');
 module.exports = config => {
   const electronConfig = withExpoWebpack(config);
   // electronConfig.preset = 'react-native';
-  // electronConfig.resolve.alias['react-native$'] = 'react-native-electron';
+  electronConfig.resolve.alias['react-native$'] = 'react-native-electron';
   return merge(electronConfig, {});
 };
