b94bda561d4551a31f3c0d5ba3ba56b8d6c91f49
Stephan Miller <stmiller@shamrocktradingcorp.com>
2020-06-27 00:20:27 -0500

    Electron Still Not Working - Before Update

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -1,5 +1,9 @@
 const { withExpoWebpack } = require('@expo/electron-adapter');
+const merge = require('webpack-merge');
 
 module.exports = config => {
-  return withExpoWebpack(config);
+  const electronConfig = withExpoWebpack(config);
+  // electronConfig.preset = 'react-native';
+  // electronConfig.resolve.alias['react-native$'] = 'react-native-electron';
+  return merge(electronConfig, {});
 };
