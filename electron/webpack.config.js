5df5c1f9fdf1d4f039f2e05ccf0cdc27219cd4a9
Stephan Miller <stmiller@shamrocktradingcorp.com>
2020-06-27 01:16:35 -0500

    Compiles for electron partially

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -3,7 +3,5 @@ const merge = require('webpack-merge');
 
 module.exports = config => {
   const electronConfig = withExpoWebpack(config);
-  // electronConfig.preset = 'react-native';
-  electronConfig.resolve.alias['react-native$'] = 'react-native-electron';
   return merge(electronConfig, {});
 };
