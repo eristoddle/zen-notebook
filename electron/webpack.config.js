7dced1b90d2bcd9e72a3d2f32fa8e77197d0c666
eristoddle <stephanmil@gmail.com>
2021-09-06 20:18:11 -0500

    start over

--- electron/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const { withExpoWebpack } = require('@expo/electron-adapter');
-const merge = require('webpack-merge');
-
-module.exports = config => {
-  const electronConfig = withExpoWebpack(config);
-  return merge(electronConfig, {});
-};
