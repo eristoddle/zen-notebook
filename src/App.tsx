72649aa6bed8fda89a71662148212b9ba80341be
Stephan Miller <stephanmil@gmail.com>
2022-01-08 20:20:58 -0600

    Add Safe Area Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppStore from './state/index';
 import AppStackNavigator from './navigation/AppStackNavigator';
 import ErrorBoundary from './components/ErrorBoundary';
@@ -9,15 +10,17 @@ import { navigationRef } from './navigation/RootNavigation';
 const App: FC = () => {
   return (
     <Provider store={AppStore}>
-      <ErrorBoundary
-        resetState={() => {
-          console.log('resetState');
-        }}
-      >
-        <NavigationContainer ref={navigationRef}>
-          <AppStackNavigator />
-        </NavigationContainer>
-      </ErrorBoundary>
+      <SafeAreaProvider>
+        <ErrorBoundary
+          resetState={() => {
+            console.log('resetState');
+          }}
+        >
+          <NavigationContainer ref={navigationRef}>
+            <AppStackNavigator />
+          </NavigationContainer>
+        </ErrorBoundary>
+      </SafeAreaProvider>
     </Provider>
   );
 };
