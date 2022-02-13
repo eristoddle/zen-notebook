63bd9436298bbc4bbd7639bc904229fac2ac1c49
Stephan Miller <stephanmil@gmail.com>
2022-02-13 14:04:02 -0600

    Add Markdown Library

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,14 +9,4 @@ const Home: FC = () => {
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     flexDirection: 'column',
-//   },
-// });
-
 export default Home;
